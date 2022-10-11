9187be0c44278abaa0f1ca4c7088d6bf8cd0ea5f
Pardeep Klair <106414050+pardeepklair@users.noreply.github.com>
2022-10-10 21:10:22 -0700

    first commit

--- q2.cpp
+++ q2.cpp
@@ -3,12 +3,31 @@
 using namespace std;
 
 // ******************************
-// Your code here
+void getInput(int &, int &, int &);
+
 // ******************************
 
 // ******************************
 // this function will help you pass the test. When you print the result with this function, you will get the same output format
-void printResult(int n1, int n2, int n3, int min)
-{
+
+
+
+void getInput(int &n1, int &n2, int &n3) {
+	cout << "Enter Number 1: ";
+	cin >> n1;
+	cout << "Enter Number 2: ";
+	cin >> n2;
+	cout << "Enter Number 3: ";
+	cin >> n3;
+
+}
+
+
+
+
+
+
+void printResult(int n1, int n2, int n3, int min) {
 	cout << n1 << " " << n2 << " " << n3 << " " << min << " " << endl;
+
 }
